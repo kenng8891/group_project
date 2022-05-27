8880ea65aea3587c095e3f3821a5a8c2525a826c
TJ <tdegirol@gmail.com>
2022-05-26 20:40:14 -0400

    added in if/elseif for codes & display output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
 key = '48faf7c466632bcab0303bde';
 //And here's an example request: https://v6.exchangerate-api.com/v6/48faf7c466632bcab0303bde/latest/USD
-var amountOrigin = document.getElementById('amount-origin').value;
-var amountDestination = document.getElementById('amount-destination').textContent;
-var currencyOrigin = document.getElementById('currency-origin');
-var currencyDestination = document.getElementById('currency-destination');
+
+var amountDestination = document.getElementById('amount-destination');
 var convertBtn = document.getElementById('convert-button');
-var selectedCurrency = currencyOrigin.options[currencyOrigin.selectedIndex].value;
-var selectedDestination = currencyDestination.options[currencyDestination.selectedIndex].value;
 
 function getAPI(){
+    var currencyOrigin = document.getElementById('currency-origin');
+    var selectedCurrency = currencyOrigin.options[currencyOrigin.selectedIndex].value;
     var exchangeAPI = 'https://v6.exchangerate-api.com/v6/'+key+'/latest/'+selectedCurrency;
     fetch(exchangeAPI)
         .then(function(response){
@@ -16,23 +14,20 @@ function getAPI(){
         })
         .then(function(exchData){
             console.log(exchData);
-            selectedDestination = window[selectedDestination]
-            console.log(exchData.conversion_rates.selectedDestination);
-            console.log(selectedDestination);
-            // console.log(exchangeRate);
-            // console.log(amountOrigin);
-            // console.log(amountDestination);
-            var exchangeRate = exchData.conversion_rates.selectedDestination;
-            amountDestination = amountOrigin*exchangeRate;
+            var currencyDestination = document.getElementById('currency-destination');
+            var selectedDestination = currencyDestination.options[currencyDestination.selectedIndex].value;
+            if (selectedDestination === 'EUR'){
+                var exchangeRate = exchData.conversion_rates.EUR
+            }else if (selectedDestination === 'USD'){
+                var exchangeRate = exchData.conversion_rates.USD
+            }else if (selectedDestination === 'CAD'){
+                var exchangeRate = exchData.conversion_rates.CAD
+            }else if (selectedDestination === 'GBP'){
+                var exchangeRate = exchData.conversion_rates.GBP
+            }
+            var amountOrigin = document.getElementById('amount-origin').value;
+            const exchNum = amountOrigin*exchangeRate;
+            amountDestination.value = exchNum;
         })
 }
-
 convertBtn.addEventListener('click', getAPI);
-
-// if (selectedDestination === 'EUR'){
-//     exchData.conversion_rates.EUR
-// }else if (selectedDestination === 'USD'){
-//     exchData.conversion_rates.USD
-// }
-//created js
-//HTML Changes: added id tags, changed option values to match currency codes, and text of countries
